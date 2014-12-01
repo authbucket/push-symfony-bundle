6d00998748c640ad8d453a737f67f166b1e71cd5
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-12-01 22:41:45 +0800

    Always load TestBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
         $bundles = array(
             new AuthBucket\Bundle\OAuth2Bundle\AuthBucketOAuth2Bundle(),
             new AuthBucket\Bundle\PushBundle\AuthBucketPushBundle(),
+            new AuthBucket\Bundle\PushBundle\Tests\TestBundle\TestBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
@@ -27,10 +28,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
         );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new AuthBucket\Bundle\PushBundle\Tests\TestBundle\TestBundle();
-        }
 
         return $bundles;
     }
